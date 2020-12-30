510e482f431e047b9318c58f5e16d3482b6e4991
e a <nanteneus@hotmail.co.jp>
2020-12-30 10:31:08 +0000

    show mock todo data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,50 @@
 use actix_web::ResponseError;
 use actix_web::{get, App, HttpResponse, HttpServer};
+use askama::Template;
 use thiserror::Error;
 
+struct TodoEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
 impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
+    let html = IndexTemplate {
+        entries: get_entries(),
+    };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
+}
+
+fn get_entries() -> Vec<TodoEntry> {
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    entries
+}
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
@@ -14,11 +53,4 @@ async fn main() -> Result<(), actix_web::Error> {
         .run()
         .await?;
     Ok(())
-}
-
-#[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello world!";
-
-    Ok(HttpResponse::Ok().body(response_body))
 }
